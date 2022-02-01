ceafaa7b5711ae15ee9bed26fbe7a42118c2387e
Anshitajoshi <you@example.com>
2022-02-01 08:48:54 +0530

    html file

---
+++ JS/day7/script.js
@@ -0,0 +1,19 @@
+console.log("hello javascript")
+
+var a=10;
+console.log(a);
+
+document.getElementById('one').innerHTML=a;
+var b=10.5;
+console.log(b);
+ var c='indore';
+ console.log(a+b);
+ console.log(a+c);
+ console.log(10==="10");
+ console.log(10===10);
+ a=10;
+ a+=5
+ console.log(a);
+
+ status1=1
+ status1==0?console.log("login"):console.log("logout");
---
+++ JS/day8/script.js
@@ -0,0 +1,7 @@
+var i=1;
+var s=" ";
+while(i<=10){
+    s = s + i + "<br>"
+    i+=1;
+}
+document.getElementById('one').innerHTML=s;
